537a677b9300c8feab1e16679556cd94788f47d1
ImKhisam <imkhisam@gmail.com>
2023-02-25 16:20:42 +0600

    bb button show pdf

--- mysite/content_for_evrbd/urls.py
+++ mysite/content_for_evrbd/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('BB/', BB.as_view(), name='BB'),
+    path('BB/', pdf_view, name='BB'),
     ]
--- mysite/content_for_evrbd/views.py
+++ mysite/content_for_evrbd/views.py
@@ -1,5 +1,7 @@
+import os
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import FileResponse, Http404
 
 
 class BB(TemplateView):
@@ -9,3 +11,13 @@ class BB(TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'BB'
         return context
+
+
+def pdf_view(request):
+    module_dir = os.path.dirname(__file__)
+    filepath = os.path.join(module_dir, 'media/2015.pdf')
+
+    try:
+        return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
